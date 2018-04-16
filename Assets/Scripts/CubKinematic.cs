83879756ae2459701ecaf2d2b518ee3ed87396ba
JooYoo <jooyoo@outlook.com>
2018-04-16 18:49:32 +0200

    created small project

---
+++ Assets/Scripts/CubKinematic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubKinematic : MonoBehaviour
+{
+    private Rigidbody rb;
+	void Start ()
+	{
+	    rb = GetComponent<Rigidbody>();
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb.AddForce(Vector3.forward);
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Pressed S");
+            rb.isKinematic = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("Pressed G");
+            rb.isKinematic = false;
+        }
+    }
+}
